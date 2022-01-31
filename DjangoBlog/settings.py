b5fc71e16848b15925a5b6ba67acde510d4abb21
MatiasCra <cravchikm@gmail.com>
2022-01-31 12:14:09 -0300

    prepare for production

--- DjangoBlog/settings.py
+++ DjangoBlog/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "static/"
-
+STATIC_ROOT = BASE_DIR / "static/"
 
 # Media
 MEDIA_URL = "media/"
--- DjangoBlog/urls.py
+++ DjangoBlog/urls.py
@@ -19,7 +19,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.shortcuts import redirect, render
 
-
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("accounts.urls")),
@@ -42,3 +41,4 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
